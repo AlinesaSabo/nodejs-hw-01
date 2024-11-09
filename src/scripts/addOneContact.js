b807d2602b6bc918ff53ffc5d6af1c4b15131471
Alinesa <alinesa322@gmail.com>
2024-11-09 14:00:15 +0200

    add

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  contacts.push(createFakeContact());
+  await writeContacts(contacts);
+};
 
 addOneContact();
